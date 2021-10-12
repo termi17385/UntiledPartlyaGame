82d7166c0f6b2b51eaa0f4c3f6d170cc5bea5a54
rhyssaunders44 <78834755+rhyssaunders44@users.noreply.github.com>
2021-10-12 14:05:05 +1100

    yeet

---
+++ untipargam/Assets/PlayerScripts/P_Casing.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P_Casing : MonoBehaviour
+{
+
+    private int deathTime = 40;
+    void Start()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.AddForce(new Vector3(2,0,0),ForceMode.Impulse);
+        StartCoroutine(Death());
+    }
+
+    private IEnumerator Death()
+    {
+        yield return new WaitForSeconds(deathTime);
+        Destroy(gameObject);
+    }
+
+}
