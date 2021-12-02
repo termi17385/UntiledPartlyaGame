7bb2a5960ed95826d018011c0551f4fe258c8b93
Kieran <kieranc.inbox@gmail.com>
2021-12-02 11:30:59 +1100

    maps work

--- Assets/UtiledPartlyaGame/Scripts/MapManager.cs
+++ Assets/UtiledPartlyaGame/Scripts/MapManager.cs
@@ -1,19 +1,31 @@
+using Mirror;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapManager : MonoBehaviour
+public class MapManager : NetworkBehaviour
 {
     [SerializeField] private GameObject map0;
     [SerializeField] private GameObject map1;
     // Start is called before the first frame update
     void Start()
     {
+        TurnOffMap();
+    }
+
+
+    [Command(requiresAuthority = false)] 
+    public void TurnOffMap()
+    {
         if(GameManager.instance != null)
         {
             if(GameManager.instance.GameMap == 1)
             {
                 map0.SetActive(false);
+            }
+            else
+            {
+                map1.SetActive(false);
             }
         }
         else
